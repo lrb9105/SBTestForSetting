21870fc48d595f9a14f6c7d3284457efbc640344
ryeong <leeryon9105@naver.com>
2020-05-20 15:00:06 +0900

    First Commit

---
+++ SBTestForSetting/src/main/java/com/ryeong/test/SbTestForSettingApplication.java
@@ -0,0 +1,13 @@
+package com.ryeong.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTestForSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTestForSettingApplication.class, args);
+	}
+
+}
